751897568def6c3e3173951f727fe894a0de4865
Raikonif <raikonif@gmail.com>
2024-01-02 21:27:35 -0400

    menu behavior modified

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,9 @@
 import { GiMicroscope } from "react-icons/gi";
+import { useState } from "react";
 function Menu() {
-  const isMobile = () => {
-    if (window.innerWidth < 768) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  const [selected, setSelected] = useState<string>("HOME");
+  const options: string[] = ["HOME", "ABOUT", "BLOG"];
+
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex bg-violet-600 py-4 md:px-10 px-7 justify-between">
@@ -15,9 +12,19 @@ function Menu() {
           <h3 className="text-white">PANDY BLOG</h3>
         </div>
         <ul className="flex w-auto items-center justify-between">
-          <li className="m-2 mx-5 text-2xl text-white">Home</li>
-          <li className="m-2 mx-5 text-2xl text-white">About</li>
-          <li className="m-2 mx-5 text-2xl text-white">Blog</li>
+          {options.map((option) => (
+            <li
+              key={option}
+              className={`${
+                selected === option ? "bg-fuchsia-600 rounded-xl" : ""
+              } m-2 p-2 text-2xl text-white active:text-fuchsia-600 active:bg-violet-400 active:rounded-lg font-[Arial]`}
+              onClick={() => {
+                setSelected(option);
+              }}
+            >
+              {option}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
