1975f35b3e1de95bf59d387a01f6f8347f299348
Raikonif <raikonif@gmail.com>
2024-01-05 15:30:15 -0400

    search no items found added

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -5,22 +5,33 @@ interface ArticleListProps {
   articles: article[];
 }
 function ArticleList({ articles }: ArticleListProps) {
+  const articlesLength = articles.length;
   return (
-    <ul className="mx-3 grid sm:grid-cols-2 lg:grid-cols-3">
-      {articles.map((article: article) => (
-        <li className="mx-2" key={article.id}>
-          <Article
-            id={article.id}
-            title={article.title}
-            author={article.author}
-            content={article.content}
-            date={article.date}
-            images={article.images}
-            type={article.type}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="mx-3 grid sm:grid-cols-2 lg:grid-cols-3">
+        {articlesLength > 0 &&
+          articles.map((article: article) => (
+            <li className="mx-2" key={article.id}>
+              <Article
+                id={article.id}
+                title={article.title}
+                author={article.author}
+                content={article.content}
+                date={article.date}
+                images={article.images}
+                type={article.type}
+              />
+            </li>
+          ))}
+      </ul>
+      {articlesLength === 0 && (
+        <div className="w-full justify-center items-center flex mt-10">
+          <h1 className="text-center text-2xl h-full w-full">
+            No articles found
+          </h1>
+        </div>
+      )}
+    </>
   );
 }
 
